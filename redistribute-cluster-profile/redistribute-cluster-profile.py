4ccc7cfbcdc83b37d78f9032031202e01f1379ad
Bear <bechen@redhat.com>
2023-10-31 10:31:14 -0230

    Add statistics

--- redistribute-cluster-profile/redistribute-cluster-profile.py
+++ redistribute-cluster-profile/redistribute-cluster-profile.py
@@ -2,15 +2,15 @@ import ruamel.yaml
 import os
 import sys
 import io
+import random
 
 existing = ['gcp', 'gcp-openshift-gce-devel-ci-2']
 added = ['gcp-3']
 
 def random_choose_cluster_profile():
-    import random
-    return random.choice(existing + added)
+    return random.choice(added)
 
-def update_cluster_profile(yaml_content):
+def update_cluster_profile(yaml_content, statistics={}):
     try:
         yaml = ruamel.yaml.YAML()
         yaml.preserve_quotes = True
@@ -20,8 +20,13 @@ def update_cluster_profile(yaml_content):
             for test in data['tests']:
                 if 'steps' in test and 'cluster_profile' in test['steps']:
                     if test['steps']['cluster_profile'] in existing:
-                        test['steps']['cluster_profile'] = random_choose_cluster_profile()
-                        modified = True
+                        if test['steps']['cluster_profile'] not in statistics:
+                            statistics[test['steps']['cluster_profile']] = 0
+                        statistics[test['steps']['cluster_profile']] += 1
+                        target_ratio = len(added) / (len(existing) + len(added))
+                        if random.random() < target_ratio / len(existing):
+                            test['steps']['cluster_profile'] = random_choose_cluster_profile()
+                            modified = True
         if modified:
             stream = io.StringIO()
             yaml.dump(data, stream)
@@ -35,6 +40,7 @@ def update_cluster_profile(yaml_content):
 
 def update_yaml_files(directory):
     yaml_ext = {'.yaml', '.yml'}
+    statistics = {}
     for root, dirs, files in os.walk(directory):
         for file in files:
             print('Processing file:', file)
@@ -42,12 +48,15 @@ def update_yaml_files(directory):
                 file_path = os.path.join(root, file)
                 with open(file_path, 'r', encoding='utf-8') as f:
                     content = f.read()
-                updated_content = update_cluster_profile(content)
+                updated_content = update_cluster_profile(content, statistics)
                 if updated_content:
                     with open(file_path, 'w', encoding='utf-8') as f:
                         f.write(updated_content)
                 else:
                     print(f"Skipped file due to error: {file_path}")
+    print("Statistics:")
+    for cluster_profile, count in statistics.items():
+        print(f"{cluster_profile}: {count}")
 
 if __name__ == "__main__":
     directory = sys.argv[1] if len(sys.argv) > 1 else None
